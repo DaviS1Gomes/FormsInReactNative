ba9b3921e18d6683fce48ca905828adee9b8d67e
Davi Gomes <daviszgomes@gmail.com>
2024-12-05 18:33:22 -0300

    Added Custom Font Inter

---
+++ src/screens/Shipping.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import * as S from '../styles/Header';
+
+function Shipping(): React.JSX.Element {
+  return (
+    <S.Header>
+      <Text> Hello World </Text>
+    </S.Header>
+  );
+}
+
+export default Shipping;
---
+++ src/styles/Header.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background-color: #fff;
+`;
+export const Header = styled.View`
+  background-color: #000;
+`;
+export const MainTextHeader = styled.Text`
+  color: #fff;
+  font-family: 'Inter', sans-serif;
+`;
